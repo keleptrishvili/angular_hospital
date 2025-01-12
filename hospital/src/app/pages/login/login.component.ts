a42aee209541b3f54c079a5fec45f0bb83c56288
lizi keleptrishvili <lizi.keleptrishvili@gau.edu.ge>
2025-01-12 23:28:27 +0400

    1st step of login

---
+++ hospital/src/app/pages/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule], 
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'] 
+})
+export class LoginComponent {
+  form: FormGroup; 
+
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]] 
+    });
+  }
+}
